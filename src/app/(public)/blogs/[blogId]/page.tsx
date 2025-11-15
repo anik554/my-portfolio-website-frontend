5b8f10b5008d93d60d8f0b929b4faf264e0854e3
anik924 <anikimtiaz924@gmail.com>
2025-11-15 23:27:58 +0600

    ssg function generated

--- src/app/(public)/blogs/[blogId]/page.tsx
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -1,5 +1,14 @@
 import BlogDetailsCard from "@/components/modules/Blogs/BlogDetailsCard";
 
+export const generateStaticParams =async()=>{
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/blog`)
+  const {data:blogs}= await res.json()
+
+  return blogs?.map((blog:{id:number})=>({
+    blogId: String(blog.id)
+  }))
+}
+
 const BlogDetailsPage = async ({
   params,
 }: {
@@ -11,7 +20,6 @@ const BlogDetailsPage = async ({
   const blog = await (await res).json()
   return (
     <div className="py-30 px-4 max-w-7xl mx-auto">
-      <h1>BlogDetailsPage</h1>
       <BlogDetailsCard blog={blog} />
     </div>
   );
